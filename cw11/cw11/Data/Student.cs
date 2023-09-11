51947264a685758af049f535ea5f4c82fe1e30f6
Bambi54 <108064906+Bambi54@users.noreply.github.com>
2023-09-11 12:04:27 +0200

    Add files via upload

---
+++ cw11/cw11/Data/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace cw11.Data
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+        public string Studies { get; set; } = string.Empty;
+        public string AvatarURL { get; set; } = string.Empty;
+    }
+}
---
+++ cw11/cw11/Data/StudentService.cs
@@ -0,0 +1,60 @@
+﻿namespace cw11.Data
+{
+    public interface IStudentService
+    {
+        public ICollection<Student> GetStudents();
+        public Student? GetStudent(int id);
+    }
+    public class StudentService: IStudentService
+    {
+        private readonly ICollection<Student> _students;
+
+        public StudentService()
+        {
+            _students = new List<Student>()
+            {
+                new Student
+                {
+                    ID = 1,
+                    FirstName = "Krystian",
+                    LastName = "Szturemski",
+                    Birthdate = DateTime.Parse("01/01/2000"),
+                    Studies = "Informatyka",
+                    AvatarURL = "https://img.freepik.com/darmowe-wektory/awatar-postaci-biznesmen-na-bialym-tle_24877-60111.jpg?w=2000"
+
+                },
+                new Student
+                {
+                    ID = 2,
+                    FirstName = "Jan",
+                    LastName = "Zalewski",
+                    Birthdate = DateTime.Parse("01/01/1990"),
+                    Studies = "Nowe Media",
+                    AvatarURL = "https://img.freepik.com/darmowe-wektory/awatar-postaci-biznesmen-na-bialym-tle_24877-60111.jpg?w=2000"
+
+                },
+                new Student
+                {
+                    ID = 3,
+                    FirstName = "Aleksander",
+                    LastName = "Babij",
+                    Birthdate = DateTime.Parse("01/01/2001"),
+                    Studies = "Zarządzanie",
+                    AvatarURL = "https://img.freepik.com/darmowe-wektory/awatar-postaci-biznesmen-na-bialym-tle_24877-60111.jpg?w=2000"
+
+                }
+            };
+        }
+
+        public ICollection<Student> GetStudents()
+        {
+            return _students;
+        }
+
+        public Student? GetStudent(int id)
+        {
+            return _students.FirstOrDefault(e => e.ID == id);
+        }
+
+    }
+}
